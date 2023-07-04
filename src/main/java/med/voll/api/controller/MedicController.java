6dbe46a0aa41f964fafafe5bae1d36c07b054f3d
Marcio Moura <murilomoura2005@gmail.com>
2023-07-04 01:46:37 -0300

    Created Project

---
+++ src/main/java/med/voll/api/controller/MedicController.java
@@ -0,0 +1,16 @@
+package med.voll.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/medics")
+public class MedicController {
+
+	@PostMapping
+	public void register() {
+		
+	}
+	
+}
